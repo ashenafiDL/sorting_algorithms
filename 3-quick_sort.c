4c96f874061217ac62b45938a57a2a77b126073b
Ashenafi Debella <ashenafi.debella101@gmail.com>
2022-11-04 12:56:13 +0300

    add missing documentations and update logic

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
  * quick_sort - sorts an array of integers in ascending order
@@ -24,6 +25,8 @@ void quick_sort(int *array, size_t size)
  *
  * @arr: the sub array to sorted
  * @low: first index of sub array
+ * @size: the size of the array
+ *
  * @high: last index of sub array
  */
 void sort(int *arr, size_t size, int low, int high)
@@ -38,6 +41,16 @@ void sort(int *arr, size_t size, int low, int high)
 	}
 }
 
+/**
+ * partition - divides an an array into two using Lomuto's partition scheme
+ *
+ * @arr: the array to be partitioned
+ * @size: size of the array
+ * @low: left index of array
+ * @high: right index of the array
+ *
+ * Return: (int) the index of the pivot element
+ */
 int partition(int *arr, size_t size, int low, int high)
 {
 	int pivot = arr[high],
@@ -48,17 +61,30 @@ int partition(int *arr, size_t size, int low, int high)
 		if (arr[low] < pivot)
 		{
 			i++;
-			swap_array(arr, i, low);
-			print_array(arr, size);
+			if (i != low)
+			{
+				swap_array(arr, i, low);
+				print_array(arr, size);
+			}
 		}
 		low++;
 	}
 
-	swap_array(arr, i + 1, high);
-	print_array(arr, size);
+	if (i + 1 != high)
+	{
+		swap_array(arr, i + 1, high);
+		print_array(arr, size);
+	}
 	return (i + 1);
 }
 
+/**
+ * swap_array - swaps two array elements
+ *
+ * @array: the array
+ * @a: the first element
+ * @b: the second element
+ */
 void swap_array(int *array, int a, int b)
 {
 	int temp;
